8ff31eafb4f3b8cb692da0c29ce7a011cce8e98f
SavoSya <savostikov.artem@gmail.com>
2024-04-02 00:30:37 +0300

    RegPage designed and confirm pass with eye added

--- lib/pages/registration.dart
+++ lib/pages/registration.dart
@@ -1,16 +1,23 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:mama_kris/wave.dart'; // Убедитесь, что wave.dart содержит SineWaveWidget
+import 'package:mama_kris/wave.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:google_sign_in/google_sign_in.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 
 
-class RegistrationPage extends StatelessWidget {
+class RegistrationPage extends StatefulWidget {
+  @override
+  _RegistrationPageState createState() => _RegistrationPageState();
+}
+
+class _RegistrationPageState extends State<RegistrationPage> {
   final TextEditingController emailController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
+  final TextEditingController confirmPasswordController = TextEditingController(); // Для повторного ввода пароля
   final FirebaseAuth _auth = FirebaseAuth.instance;
+  bool _obscureText = true;
+  bool _obscureTextConf = true;
 
   Future<bool?> signInWithGoogle() async {
     try {
@@ -41,42 +48,88 @@ class RegistrationPage extends StatelessWidget {
     Size screenSize = MediaQuery.of(context).size;
     double width = screenSize.width;
     double height = screenSize.height;
+    double TextMultiply = min(width/360, height/800);
+    double VerticalMultiply = height/800;
+    double HorizontalMultiply = width/360;
 
     return Scaffold(
-      body: SingleChildScrollView( // Добавляем прокрутку для поддержки маленьких экранов
+      body: SingleChildScrollView(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
           children: <Widget>[
             Stack(
               children: [
-                SineWaveWidget(verticalOffset: 300),
+                SineWaveWidget(verticalOffset:  340*VerticalMultiply),
                 Align(
                   alignment: Alignment.center,
                   child: Padding(
-                    padding: EdgeInsets.only(top: 38),
-                    child: SvgPicture.asset('images/logo_named.svg'),
+                    padding: EdgeInsets.only(top: 58*VerticalMultiply),
+                    child: SvgPicture .asset(
+                      'images/logo_named.svg',
+                      width: 220*HorizontalMultiply, // Ширина в пикселях
+                      height: 224*VerticalMultiply, // Высота в пикселях
+                    ),
                   ),
                 ),
               ],
             ),
-            const Padding(
-              padding: EdgeInsets.only(
-                  left: 32.0, top: 70.0, right: 32.0, bottom: 22.0),
-              child: Text(
-                'Пожалуйста, зарегистрируйтесь',
-                style: TextStyle(fontSize: 30,
-                    color: Color(0xFF343434),
-                    fontFamily: 'Inter',
-                    fontWeight: FontWeight.w700),
-                textAlign: TextAlign.left,
+            Padding(
+              padding:  EdgeInsets.only(left: 32*HorizontalMultiply, top: 98*VerticalMultiply, right:0, bottom:0), // Общий отступ для группы текстов
+              child:  Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  'Пожалуйста,',
+                  style: TextStyle(fontSize: 32*TextMultiply, fontFamily: 'Inter', fontWeight: FontWeight.w700, color: const Color(0xFF343434), height: 1,),
+                ),
               ),
             ),
-            _buildTextField(emailController, 'Email', false),
-            _buildTextField(passwordController, 'Пароль', true),
+            Padding(
+              padding:  EdgeInsets.only(left: 32*HorizontalMultiply, top: 0, right:0, bottom:0), // Общий отступ для группы текстов
+              child:  Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  'зарегистрируйтесь',
+                  style: TextStyle(fontSize: 32*TextMultiply, fontFamily: 'Inter', fontWeight: FontWeight.w700, color: const Color(0xFF343434), height: 1,),
+                ),
+              ),
+            ),
+            Padding(
+              padding:  EdgeInsets.only(left: 32*HorizontalMultiply, top: 16*VerticalMultiply , right:0, bottom:0), // Общий отступ для группы текстов
+              child:  Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  'email',
+                  style: TextStyle(fontSize: 13*TextMultiply, fontFamily: 'Inter', fontWeight: FontWeight.w600, color: const Color(0xFF343434), height: 1,),
+                ),
+              ),
+            ),
+            _buildEmailField(emailController, 32*HorizontalMultiply, 5*VerticalMultiply),
+            Padding(
+              padding:  EdgeInsets.only(left: 32*HorizontalMultiply, top: 8*VerticalMultiply, right:0, bottom:0), // Общий отступ для группы текстов
+              child:  Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  'пароль',
+                  style: TextStyle(fontSize: 13*TextMultiply, fontFamily: 'Inter', fontWeight: FontWeight.w600, color: const Color(0xFF343434), height: 1,),
+                ),
+              ),
+            ),
+            _buildPassField(passwordController, _obscureText, 32*HorizontalMultiply, 5*VerticalMultiply, true),
+            Padding(
+              padding:  EdgeInsets.only(left: 32*HorizontalMultiply, top: 8*VerticalMultiply, right:0, bottom:0), // Общий отступ для группы текстов
+              child:  Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  'подтвердите пароль',
+                  style: TextStyle(fontSize: 13*TextMultiply, fontFamily: 'Inter', fontWeight: FontWeight.w600, color: const Color(0xFF343434), height: 1,),
+                ),
+              ),
+            ),
+            _buildPassField(confirmPasswordController, _obscureTextConf, 32*HorizontalMultiply, 5*VerticalMultiply, false),
 
 
             Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 32.0, vertical: 20.0),
+              padding:  EdgeInsets.only(left: 32*HorizontalMultiply, top: 14*VerticalMultiply, right: 32*HorizontalMultiply, bottom:0), // Общий отступ для группы текстов
               child: Container(
                 decoration: BoxDecoration(
                   gradient: const LinearGradient(
@@ -84,7 +137,7 @@ class RegistrationPage extends StatelessWidget {
                     begin: Alignment.topCenter,
                     end: Alignment.bottomCenter,
                   ),
-                  borderRadius: BorderRadius.circular(12), // Скругление углов
+                  borderRadius: BorderRadius.circular(12*TextMultiply), // Скругление углов
                 ),
               child: ElevatedButton(
                 style: ElevatedButton.styleFrom(
@@ -93,112 +146,16 @@ class RegistrationPage extends StatelessWidget {
                   shape: RoundedRectangleBorder(
                     borderRadius: BorderRadius.circular(12), // Скругление углов
                   ),
-                  minimumSize: Size(double.infinity, 60), // Растягиваем кнопку на всю ширину с высотой 60
-                  padding: EdgeInsets.symmetric(vertical: 15),
+                  minimumSize: Size(double.infinity, 60*VerticalMultiply), // Растягиваем кнопку на всю ширину с высотой 60
+                  padding: EdgeInsets.only(top: 23*VerticalMultiply, bottom:23*VerticalMultiply),
                 ),
-                onPressed: () async {
-                  try {
-                    // Регистрация пользователя с помощью email и пароля
-                    final UserCredential userCredential = await _auth.createUserWithEmailAndPassword(
-                      email: emailController.text,
-                      password: passwordController.text,
-                    );
-
-                    // Проверка, успешно ли прошла регистрация
-                    if (userCredential.user != null) {
-                      // Регистрация прошла успешно
-                      print('Пользователь успешно зарегистрирован!');
-
-                      // Отправка письма с подтверждением на email пользователя
-                      await userCredential.user!.sendEmailVerification();
-
-                      // Перенаправление пользователя на страницу верификации
-                      Navigator.pushReplacementNamed(context, '/verification');
-                    } else {
-                      // Регистрация не удалась
-                      print('Регистрация не удалась.');
-                    }
-                  } on FirebaseAuthException catch (e) {
-                    if (e.code == 'email-already-in-use') {
-                      // Email уже используется
-                      ScaffoldMessenger.of(context).showSnackBar(
-                        const SnackBar(
-                          content: Text('Такой пользователь уже зарегистрирован!'),
-                          backgroundColor: Colors.red,
-                        ),
-                      );
-                    }
-                  } catch (e) {
-                    // Обработка других ошибок
-                    print('Ошибка: $e');
-                  }
-                },
-                child: const Text(
-                  'Зарегистрироваться',
-                  style: TextStyle(fontSize: 18, color: Colors.white),
+                onPressed:  _register,
+                child:  Text(
+                  'ЗАРЕГИСТРИРОВАТЬСЯ',
+                  style: TextStyle(fontSize: 14*TextMultiply, color: const Color(0xFFFFFFFF), fontFamily: 'Inter', fontWeight: FontWeight.w700),
                 ),
               ),
             ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 32.0, vertical: 8.0),
-              child: ElevatedButton.icon(
-                icon: const FaIcon(
-                  FontAwesomeIcons.google,
-                  color: Colors.red,
-                ),
-                label: Text(""),
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.white,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(12),
-                  ),
-                  minimumSize: const Size(60, 60),
-                ),
-                onPressed: () async {
-                  final bool? isNewUser = await signInWithGoogle();
-                  if (isNewUser == true) {
-                    // Новый пользователь
-                    Navigator.pushReplacementNamed(context, '/choice');
-                  } else if (isNewUser == false) {
-                    // Пользователь уже существует, получаем дополнительные данные из Firestore
-                    final User? user = FirebaseAuth.instance.currentUser;
-                    if (user != null) {
-                      final uid = user.uid;
-                      final docSnapshot = await FirebaseFirestore.instance.collection('choices').doc(uid).get();
-                      final docSnapshot2 = await FirebaseFirestore.instance.collection('jobSearches').doc(uid).get();
-
-                      // Проверяем, содержит ли документ информацию о выборе пользователя
-                      if (docSnapshot.exists && docSnapshot.data()!.containsKey('choice')) {
-                        final choice = docSnapshot.data()!['choice'];
-
-                        // Перенаправляем пользователя в зависимости от его выбора
-                        if (choice == 'ищу работу' && docSnapshot2.exists && docSnapshot2.data()!.containsKey('employerId')) {
-                          Navigator.pushNamed(context, '/tinder'); // Перенаправление на страницу поиска работы
-                        }
-                        else if (choice == 'ищу работу') {
-                          Navigator.pushNamed(context, '/search'); // Перенаправление на страницу с вакансиями
-                        }
-                        else if (choice == 'есть вакансии') {
-                          Navigator.pushNamed(context, '/empl_list'); // Перенаправление на страницу с вакансиями
-                        } else {
-                          Navigator.pushReplacementNamed(context, '/choice');
-                        }
-                      } else {
-                        // Документ не найден или не содержит выбора
-                        Navigator.pushReplacementNamed(context, '/choice');
-                      }
-                    } else {
-                      print('Ошибка: пользователь не определён после входа через Google.');
-                    }
-                  } else {
-                    // Ошибка аутентификации или отмена входа пользователем
-                    print("Ошибка аутентификации или вход отменён пользователем");
-                  }
-
-                },
-              )
-
             ),
           ],
         ),
@@ -206,6 +163,53 @@ class RegistrationPage extends StatelessWidget {
     );
   }
 
+  Future<void> _register() async {
+    if (passwordController.text == confirmPasswordController.text) {
+      try {
+        // Регистрация пользователя с помощью email и пароля
+        final UserCredential userCredential = await _auth.createUserWithEmailAndPassword(
+          email: emailController.text,
+          password: passwordController.text,
+        );
+
+        // Проверка, успешно ли прошла регистрация
+        if (userCredential.user != null) {
+          // Регистрация прошла успешно
+          print('Пользователь успешно зарегистрирован!');
+
+          // Отправка письма с подтверждением на email пользователя
+          await userCredential.user!.sendEmailVerification();
+
+          // Перенаправление пользователя на страницу верификации
+          Navigator.pushReplacementNamed(context, '/verification');
+        } else {
+          // Регистрация не удалась
+          print('Регистрация не удалась.');
+        }
+      } on FirebaseAuthException catch (e) {
+        if (e.code == 'email-already-in-use') {
+          // Email уже используется
+          ScaffoldMessenger.of(context).showSnackBar(
+            const SnackBar(
+              content: Text('Такой пользователь уже зарегистрирован!'),
+              backgroundColor: Colors.red,
+            ),
+          );
+        }
+      } catch (e) {
+        // Обработка других ошибок
+        print('Ошибка: $e');
+      }
+    } else {
+      // Показать сообщение, если пароли не совпадают
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text('Пароли не совпадают! Пожалуйста, проверьте введённые пароли.'),
+          backgroundColor: Colors.red,
+        ),
+      );
+    }
+  }
   Widget _buildTextField(TextEditingController controller, String label, bool obscureText) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 32.0, vertical: 8.0),
@@ -233,6 +237,92 @@ class RegistrationPage extends StatelessWidget {
           ),
         ),
         keyboardType: obscureText ? TextInputType.text : TextInputType.emailAddress,
+      ),
+    );
+  }
+
+  Widget _buildEmailField(TextEditingController controller, double Hpadding, double Vpadding) {
+    Size screenSize = MediaQuery.of(context).size;
+    double width = screenSize.width;
+    double height = screenSize.height;
+    double TextMultiply = min(width/360, height/800);
+    return Padding(
+      padding:  EdgeInsets.only(left: Hpadding, top: Vpadding, right:Hpadding, bottom:0), // Общий отступ для группы текстов
+      child: TextField(
+        controller: controller,
+        obscureText: false,
+        decoration: InputDecoration(
+          floatingLabelBehavior: FloatingLabelBehavior.always, // Лейбл всегда над полем
+          labelStyle: const TextStyle(color: Color(0xFF343434)), // Цвет лейбла
+          // Устанавливаем толстую рамку
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(12*TextMultiply),
+            borderSide: BorderSide(color:const Color(0xFF343434), width: 2.0*TextMultiply), // Увеличиваем ширину рамки
+          ),
+          // Также применяем стиль рамки когда поле в фокусе
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(12*TextMultiply),
+            borderSide: BorderSide(color: const Color(0xFF343434), width: 2.0*TextMultiply), // Та же толщина рамки
+          ),
+          // Стиль рамки при вводе неверных данных
+          errorBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(12*TextMultiply),
+            borderSide: BorderSide(color: Colors.red, width: 2.0*TextMultiply), // Можно изменить цвет/толщину для ошибок
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildPassField(TextEditingController controller, bool obscureText, double Hpadding, double Vpadding, bool type) {
+    Size screenSize = MediaQuery.of(context).size;
+    double width = screenSize.width;
+    double height = screenSize.height;
+    double TextMultiply = min(width/360, height/800);
+
+    return Padding(
+      padding: EdgeInsets.only(left: Hpadding, top: Vpadding, right: Hpadding, bottom: 0), // Общий отступ для группы текстов
+      child: TextField(
+        controller: controller,
+        obscureText: obscureText,
+        decoration: InputDecoration(
+          floatingLabelBehavior: FloatingLabelBehavior.always, // Лейбл всегда над полем
+          labelStyle: const TextStyle(color: Color(0xFF343434)), // Цвет лейбла
+          // Устанавливаем толстую рамку
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(12*TextMultiply),
+            borderSide: BorderSide(color:const Color(0xFF343434), width: 2.0*TextMultiply), // Увеличиваем ширину рамки
+          ),
+          // Также применяем стиль рамки когда поле в фокусе
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(12*TextMultiply),
+            borderSide: BorderSide(color: const Color(0xFF343434), width: 2.0*TextMultiply), // Та же толщина рамки
+          ),
+          // Стиль рамки при вводе неверных данных
+          errorBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(12*TextMultiply),
+            borderSide: BorderSide(color: Colors.red, width: 2.0*TextMultiply), // Можно изменить цвет/толщину для ошибок
+          ),          suffixIcon: IconButton(
+          icon: Icon(
+            // Изменяем иконку в зависимости от того, скрыт текст или нет
+            obscureText ? Icons.visibility : Icons.visibility_off,
+            color: const Color(0xFF343434),
+          ),
+          onPressed: () {
+            // Переключаем состояние отображения текста при нажатии
+            setState(() {
+              if (type) {
+                _obscureText = !_obscureText;
+              }
+              else {
+                _obscureTextConf = !_obscureTextConf;
+              };
+            });
+          },
+        ),
+          // Другие свойства InputDecoration...
+        ),
+        keyboardType: TextInputType.text,
       ),
     );
   }
