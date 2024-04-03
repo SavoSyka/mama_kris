6e89df1a5d454a95b18404a6dd0b49c91f2295f2
SavoSya <savostikov.artem@gmail.com>
2024-04-03 10:21:08 +0300

    Choice designed

--- lib/pages/choice.dart
+++ lib/pages/choice.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
@@ -9,26 +11,55 @@ class ChoicePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final screenHeight = MediaQuery.of(context).size.height;
+    Size screenSize = MediaQuery.of(context).size;
+    double width = screenSize.width;
+    double height = screenSize.height;
+    double TextMultiply = min(width/360, height/800);
+    double VerticalMultiply = height/800;
+    double HorizontalMultiply = width/360;
+
 
     return Scaffold(
       backgroundColor: const Color(0xFFFCFAEE),
       body: Stack( // Используем Stack для наложения элементов
         children: <Widget>[
-          SineWaveWidget(verticalOffset: screenHeight / 2), // Волна на фоне
-          Positioned( // Позиционируем кнопку "Ищу работу" на верху экрана
-            top: 95, // Отступ сверху для размещения поверх волны
-            left: 32, // Отступ слева
-            right: 32, // Отступ справа
+          SineWaveWidget(verticalOffset: height / 2), // Волна на фоне
+
+          Align(
+            alignment: Alignment.center,
+            child: Padding(
+              padding: EdgeInsets.only(top: 15*VerticalMultiply),
+              child: SvgPicture .asset(
+                'images/logo.svg',
+                width: 220*HorizontalMultiply, // Ширина в пикселях
+                height: 224*VerticalMultiply, // Высота в пикселях
+              ),
+            ),
+          ),
+
+          Padding( // Позиционируем кнопку "Ищу работу" на верху экрана
+            padding: EdgeInsets.only(
+                left: 5.0*HorizontalMultiply, top: 35*VerticalMultiply, right: 32.0*HorizontalMultiply, bottom: 0.0),
+            child:IconButton(
+              icon: Icon(Icons.arrow_back),
+              onPressed: () {
+                Navigator.pop(context);
+              },
+            ),
+          ),
+
+          Padding(
+            padding: EdgeInsets.only(
+                left: 32.0*HorizontalMultiply, top: 110*VerticalMultiply, right: 32.0*HorizontalMultiply, bottom: 0.0),
             child: ElevatedButton(
               style: ElevatedButton.styleFrom(
                 backgroundColor: const Color(0xFFB7B39A), // Фон кнопки
                 foregroundColor: Colors.white, // Цвет текста кнопки
                 shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(12),
+                  borderRadius: BorderRadius.circular(12*TextMultiply),
                 ),
-                padding: const EdgeInsets.symmetric(vertical: 15),
-                minimumSize: const Size(double.infinity, 60), // Растягиваем на всю ширину
+                padding: EdgeInsets.only(top: 23*(height/800), bottom:23*(height/800)),
+                minimumSize:  Size(double.infinity, 60*TextMultiply), // Растягиваем кнопку на всю ширину с высотой 50
 
               ),
               onPressed: () async {
@@ -39,58 +70,43 @@ class ChoicePage extends StatelessWidget {
                   Navigator.pushNamed(context, '/search');
                 }
               },
-              child: const Text(
-                'Ищу работу',
-                style: TextStyle(fontSize: 18, fontWeight: FontWeight.w700),
+              child:  Text(
+                'Я ИСПОЛНИТЕЛЬ',
+                  style: TextStyle(fontSize: 14*TextMultiply, color: Color(0xFFFFFFFF), fontFamily: 'Inter', fontWeight: FontWeight.w700)
               ),
             ),
           ),
-          const Positioned(
-            top: 170, // Отступ сверху для размещения поверх волны
-            left: 45, // Отступ слева
-            right: 45, // Отступ справа
-            child: Text(
-              'Ищу работу, подработку или',
-              textAlign: TextAlign.center, // Добавляем здесь
-              style: TextStyle(fontSize: 16, fontFamily: 'Inter', fontWeight: FontWeight.w500, color: Color(0xFF343434)),
-            ),
-          ),
-
-          const Positioned(
-            top: 200, // Отступ сверху для размещения поверх волны
-            left: 45, // Отступ слева
-            right: 45, // Отступ справа
-            child: Text(
-              'задания для онлайн-заработка',
-              textAlign: TextAlign.center, // Добавляем здесь
-              style: TextStyle(fontSize: 16, fontFamily: 'Inter', fontWeight: FontWeight.w500, color: Color(0xFF343434)),
-            ),
-          ),
-
-          Align(
-            alignment: Alignment.center,
-            child: Padding(
-              padding: const EdgeInsets.only(top: 5,),
-              child: SvgPicture.asset('images/logo.svg'),
-            ),
-          ),
-       Positioned( // Позиционируем кнопку "Ищу работу" на верху экрана
-        top: 580, // Отступ сверху для размещения поверх волны
-        left: 32, // Отступ слева
-        right: 32, // Отступ справа
-        child:
           Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 32), // Аналогично для второй кнопки
+            padding: EdgeInsets.only(right: 32.0 * HorizontalMultiply, left: 32 * HorizontalMultiply, top: 186 * VerticalMultiply),
+            child:Container(
+              width: double.infinity,
+              height: 46*VerticalMultiply, // Примерная высота, убедитесь, что она достаточна
+              child: Center(
+                child: Text(
+                  'Ищу работу, подработку или\nзадания для онлайн-заработка',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 16*TextMultiply, fontFamily: 'Inter1', fontWeight: FontWeight.w500, color: Color(0xFF343434)),
+                ),
+              ),
+            ),
+          ),
+
+
+
+
+
+          Padding(
+            padding:  EdgeInsets.only(left: 32*HorizontalMultiply, top: 165*VerticalMultiply+ height/2, right:32*HorizontalMultiply, bottom:0), // Общий отступ для группы текстов
             child: ElevatedButton(
               style: ElevatedButton.styleFrom(
-                backgroundColor: const Color(0xFFB7B39A),
+                backgroundColor: const Color(0xFFB7B39A), // Фон кнопки
                 foregroundColor: Colors.white, // Цвет текста кнопки
-
                 shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(12),
+                  borderRadius: BorderRadius.circular(12*TextMultiply),
                 ),
-                padding: const EdgeInsets.symmetric(vertical: 15),
-                minimumSize: const Size(double.infinity, 60), // Растягиваем на всю ширину
+                padding: EdgeInsets.only(top: 23*(height/800), bottom:23*(height/800)),
+                minimumSize:  Size(double.infinity, 60*TextMultiply), // Растягиваем кнопку на всю ширину с высотой 50
+
               ),
               onPressed: () async {
                 User? user = FirebaseAuth.instance.currentUser;
@@ -100,30 +116,25 @@ class ChoicePage extends StatelessWidget {
                   Navigator.pushNamed(context, '/job');
                 }
               },
-              child: const Text('Есть вакансии',
-                style: TextStyle(fontSize: 18, fontWeight: FontWeight.w700),
+              child:  Text(
+                  'Я РАБОТОДАТЕЛЬ',
+                  style: TextStyle(fontSize: 14*TextMultiply, color: Color(0xFFFFFFFF), fontFamily: 'Inter', fontWeight: FontWeight.w700)
               ),
             ),
           ),
-      ),
-          const Positioned(
-            top: 650, // Отступ сверху для размещения поверх волны
-            left: 58, // Отступ слева
-            right: 58, // Отступ справа
-            child: Text(
-              'У  меня есть online-вакансия',
-              textAlign: TextAlign.center, // Добавляем здесь
-              style: TextStyle(fontSize: 16, fontFamily: 'Inter', fontWeight: FontWeight.w500, color: Color(0xFF343434)),
-            ),
-          ),
-          const Positioned(
-            top: 675, // Отступ сверху для размещения поверх волны
-            left: 58, // Отступ слева
-            right: 58, // Отступ справа
-            child: Text(
-              'или задание',
-              textAlign: TextAlign.center, // Добавляем здесь
-              style: TextStyle(fontSize: 16, fontFamily: 'Inter', fontWeight: FontWeight.w500, color: Color(0xFF343434)),
+
+          Padding(
+            padding: EdgeInsets.only(right: 32.0 * HorizontalMultiply, left: 32 * HorizontalMultiply, top: 244 * VerticalMultiply + height/2),
+            child:Container(
+              width: double.infinity,
+              height: 46*VerticalMultiply, // Примерная высота, убедитесь, что она достаточна
+              child: Center(
+                child: Text(
+                  'У меня есть online-вакансия\nили задание',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 16*TextMultiply, fontFamily: 'Inter1', fontWeight: FontWeight.w500, color: Color(0xFF343434)),
+                ),
+              ),
             ),
           ),
         ],
